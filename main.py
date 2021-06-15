a5a9925a8fe242c7a432c790c2f5119bba4b7c21
Baris Karakulak <baris.1907.krklk@gmail.com>
2021-06-16 00:29:01 +0300

    added voice record

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from src.record import RecorderPython
 from tkinter import *
 
 window=Tk()
-record=RecorderPython(window)
+record=RecorderPython(window,20)
 record.widget()
 window.mainloop()
--- src/record.py
+++ src/record.py
@@ -4,11 +4,17 @@ import cv2
 import time
 import numpy as np
 import threading
-import sys
+import sounddevice as sd
+from scipy.io.wavfile import write
+from moviepy.editor import *
+import os
+import time
+import alsaaudio, wave, numpy
 class RecorderPython():
-    def __init__(self,window):
+    def __init__(self,window,duration):
+        self.duration=duration
         self.end=False
-        self.before_time=time.time()
+        
         self.hold_down=False
         self.window=window
 
@@ -42,27 +48,70 @@ class RecorderPython():
         self.time_label.after(1,lambda:self.play())
     
     def rec(self):
+        self.before_time=time.time()
         writer=cv2.VideoWriter("selam.mp4", cv2.VideoWriter_fourcc(*"XVID"),10,pyautogui.size())
         while True:
+            duration=int(time.time()-self.before_time)
             frame=np.array(pyautogui.screenshot())
             frame=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
             writer.write(frame)
             if cv2.waitKey(1)==ord("q") or self.hold_down==True:
-                sys.exit()
                 break
-                
-            self.time_label.configure(text=int(time.time()-self.before_time))
-            print("Kaydediyor...")
+            
+            self.time_label.configure(text=str(duration))
+            if int(duration)==self.duration or int(duration)==self.duration:
+                self.thread1()
+                self.thread1()
+                break
+        
         writer.release()
         cv2.destroyAllWindows()
+        
+        self.combine()
         return 0
+    def voice_record(self):
+        
+        sampleRate=44100
+        voice=sd.rec(int(self.duration*sampleRate),channels=1,samplerate=sampleRate)
+        sd.wait()
+        
+        '''
+        inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE)
+        inp.setchannels(1)
+        inp.setrate(44100)
+        inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)
+        inp.setperiodsize(1024)
 
+        w = wave.open('test.wav', 'w')
+        w.setnchannels(1)
+        w.setsampwidth(2)
+        w.setframerate(44100)
+
+        while True:
+            l, data = inp.read()
+            a = numpy.fromstring(data, dtype='int16')
+            print numpy.abs(a).mean()
+            w.writeframes(data)
+            write("output.wav",sampleRate,voice)'''
     def thread(self):
+        t3=threading.Thread(target=lambda:self.voice_record())
+        t3.start()
         t1 = threading.Thread(target=lambda:self.rec())
         t1.start()
         t2 = threading.Thread(target=lambda:self.play())
         t2.start()
+    
     def thread1(self):
         t3=threading.Thread(target=lambda:self.stop())
         t3.start()
-        print("durduruldu...")
+        
+    def combine(self):
+        clip = VideoFileClip("selam.mp4")
+        print(time.strftime("%x %X "))
+        clip = clip.subclip(0, self.duration)
+        audioclip = AudioFileClip("output.wav").subclip(0, self.duration)
+        videoclip = clip.set_audio(audioclip)
+        print(videoclip.filename)
+        print(videoclip.ipython_display())
+        t=time.strftime('%x %X').replace(':','.').replace("/",".").replace("\\", ".")
+        os.rename("__temp__.mp4", f"{t}.mp4")
